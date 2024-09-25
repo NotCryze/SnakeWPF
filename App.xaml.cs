1e7c694a18f469a2f4d37f8dc1012370fb011669
Sammy Schröder <samm0413@elevcampus.dk>
2024-09-25 10:28:16 +0200

    Added methods to draw the game area once the content is rendered.

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,11 @@ namespace SnakeWPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.Show();
+        }
     }
 
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,52 @@ namespace SnakeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int _snakeSquareSize = 20;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            DrawGameArea();
+        }
+        private void DrawGameArea()
+        {
+            bool doneDrawingGameArea = false;
+            int nextX = 0, nextY = 0;
+            int rowCounter = 0;
+            bool isPrimaryColor = false;
+
+            while (!doneDrawingGameArea)
+            {
+                Rectangle rectangle = new Rectangle
+                {
+                    Width = _snakeSquareSize,
+                    Height = _snakeSquareSize,
+                    Fill = isPrimaryColor ? Brushes.Green : Brushes.DarkGreen
+                };
+
+                GameArea.Children.Add(rectangle);
+                Canvas.SetTop(rectangle, nextY);
+                Canvas.SetLeft(rectangle, nextX);
+
+                isPrimaryColor = !isPrimaryColor;
+                nextX += _snakeSquareSize;
+
+                if(nextX >= GameArea.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += _snakeSquareSize;
+                    rowCounter++;
+                    isPrimaryColor = rowCounter % 2 != 0;
+                }
+
+                if(nextY >= GameArea.ActualHeight)
+                {
+                    doneDrawingGameArea = true;
+                }
+            }
         }
     }
 }
