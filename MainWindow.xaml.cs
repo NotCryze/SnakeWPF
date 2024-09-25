3d5b5f6653ecdf304fa9a42cd81fcb18fd2a9781
Sammy Schröder <samm0413@elevcampus.dk>
2024-09-25 11:26:04 +0200

    Added new Snake Part class to hold each individual parts information and added a method to move the snake.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using SnakeWPF.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static SnakeWPF.MainWindow;
 
 namespace SnakeWPF
 {
@@ -16,11 +18,19 @@ namespace SnakeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        const int _snakeSquareSize = 20;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        const int _snakeSquareSize = 20;
+        private SolidColorBrush _snakeBodyBrush = Brushes.Blue;
+        private SolidColorBrush _snakeHeadBrush = Brushes.DarkBlue;
+        private List<SnakePart> _snakeParts = new List<SnakePart>();
+
+        public enum SnakeDirection { Left, Right, Up, Down };
+        private SnakeDirection _snakeDirection = SnakeDirection.Right;
+        private int _snakeLength;
 
         private void Window_ContentRendered(object sender, EventArgs e)
         {
@@ -62,6 +72,72 @@ namespace SnakeWPF
                     doneDrawingGameArea = true;
                 }
             }
+        }
+
+        private void DrawSnake()
+        {
+            foreach (SnakePart snakePart in _snakeParts)
+            {
+                if (snakePart.UiElement == null)
+                {
+                    snakePart.UiElement = new Rectangle()
+                    {
+                        Width = _snakeSquareSize,
+                        Height = _snakeSquareSize,
+                        Fill = snakePart.IsHead ? _snakeHeadBrush : _snakeBodyBrush
+                    };
+                    GameArea.Children.Add(snakePart.UiElement);
+                    Canvas.SetTop(snakePart.UiElement, snakePart.Position.Y);
+                    Canvas.SetLeft(snakePart.UiElement, snakePart.Position.X);
+                }
+            }
+        }
+
+        private void MoveSnake()
+        {
+            // Remove the last part of the snake, in preparation of the new part 
+            while (_snakeParts.Count >= _snakeLength)
+            {
+                GameArea.Children.Remove(_snakeParts[0].UiElement);
+                _snakeParts.RemoveAt(0);
+            }
+
+            // Mark all snake parts as non-head and change their color to the body color to prepare for the new head
+            foreach (SnakePart snakePart in _snakeParts)
+            {
+                (snakePart.UiElement as Rectangle).Fill = _snakeBodyBrush;
+                snakePart.IsHead = false;
+            }
+
+            // Determine in which direction to expand the snake, based on the snakes current direction  
+            SnakePart snakeHead = _snakeParts[_snakeParts.Count - 1];
+            double nextX = snakeHead.Position.X;
+            double nextY = snakeHead.Position.Y;
+            switch (_snakeDirection)
+            {
+                case SnakeDirection.Left:
+                    nextX -= _snakeSquareSize;
+                    break;
+                case SnakeDirection.Right:
+                    nextX += _snakeSquareSize;
+                    break;
+                case SnakeDirection.Up:
+                    nextY -= _snakeSquareSize;
+                    break;
+                case SnakeDirection.Down:
+                    nextY += _snakeSquareSize;
+                    break;
+            }
+
+            // Add the new head part 
+            _snakeParts.Add(new SnakePart()
+            {
+                Position = new Point(nextX, nextY),
+                IsHead = true
+            });
+            
+            // Draws the snake after it has been moved
+            DrawSnake();       
         }
     }
 }
