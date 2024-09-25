1f1ec4a38c5bc76626dcfd2b99b113179bfa9932
Sammy <samm0413@elevcampus.dk>
2024-09-25 19:54:28 +0200

    Added regions for the different methods for a nicer structure. Added the ability to generate the position of the food and draw it on the board on game start

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,11 +22,11 @@ namespace SnakeWPF
         public MainWindow()
         {
             InitializeComponent();
-            gameTickTimer.Tick += GameTickTimer_Tick;
+            _gameTickTimer.Tick += GameTickTimer_Tick;
         }
 
-        private SolidColorBrush _snakeBodyBrush = Brushes.Blue;
-        private SolidColorBrush _snakeHeadBrush = Brushes.DarkBlue;
+        private SolidColorBrush _snakeBodyColor = Brushes.Blue;
+        private SolidColorBrush _snakeHeadColor = Brushes.DarkBlue;
         private List<SnakePart> _snakeParts = new List<SnakePart>();
 
         const int SnakeSquareSize = 20;
@@ -37,13 +37,21 @@ namespace SnakeWPF
         private SnakeDirection _snakeDirection = SnakeDirection.Right;
         private int _snakeLength;
 
-        private DispatcherTimer gameTickTimer = new DispatcherTimer();
+        private DispatcherTimer _gameTickTimer = new DispatcherTimer();
+
+        private Random _rnd = new Random();
+
+        private UIElement _snakeFood = null;
+        private SolidColorBrush _foodColor = Brushes.Red;
 
         private void Window_ContentRendered(object sender, EventArgs e)
         {
             DrawGameArea();
             StartNewGame();
         }
+
+        #region Game Area
+
         private void DrawGameArea()
         {
             bool doneDrawingGameArea = false;
@@ -67,7 +75,7 @@ namespace SnakeWPF
                 isPrimaryColor = !isPrimaryColor;
                 nextX += SnakeSquareSize;
 
-                if(nextX >= GameArea.ActualWidth)
+                if (nextX >= GameArea.ActualWidth)
                 {
                     nextX = 0;
                     nextY += SnakeSquareSize;
@@ -75,12 +83,16 @@ namespace SnakeWPF
                     isPrimaryColor = rowCounter % 2 != 0;
                 }
 
-                if(nextY >= GameArea.ActualHeight)
+                if (nextY >= GameArea.ActualHeight)
                 {
                     doneDrawingGameArea = true;
                 }
             }
         }
+
+        #endregion
+
+        #region Snake Body
 
         private void DrawSnake()
         {
@@ -92,7 +104,7 @@ namespace SnakeWPF
                     {
                         Width = SnakeSquareSize,
                         Height = SnakeSquareSize,
-                        Fill = snakePart.IsHead ? _snakeHeadBrush : _snakeBodyBrush
+                        Fill = snakePart.IsHead ? _snakeHeadColor : _snakeBodyColor
                     };
                     GameArea.Children.Add(snakePart.UiElement);
                     Canvas.SetTop(snakePart.UiElement, snakePart.Position.Y);
@@ -100,6 +112,10 @@ namespace SnakeWPF
                 }
             }
         }
+
+        #endregion
+
+        #region Snake Movement
 
         private void MoveSnake()
         {
@@ -113,7 +129,7 @@ namespace SnakeWPF
             // Mark all snake parts as non-head and change their color to the body color to prepare for the new head
             foreach (SnakePart snakePart in _snakeParts)
             {
-                (snakePart.UiElement as Rectangle).Fill = _snakeBodyBrush;
+                (snakePart.UiElement as Rectangle).Fill = _snakeBodyColor;
                 snakePart.IsHead = false;
             }
 
@@ -143,9 +159,9 @@ namespace SnakeWPF
                 Position = new Point(nextX, nextY),
                 IsHead = true
             });
-            
+
             // Draws the snake after it has been moved
-            DrawSnake();       
+            DrawSnake();
         }
 
         private void GameTickTimer_Tick(object sender, EventArgs e)
@@ -153,18 +169,59 @@ namespace SnakeWPF
             MoveSnake();
         }
 
+        #endregion
+
+        #region Game Startup
+
         private void StartNewGame()
         {
             _snakeLength = SnakeStartLength;
             _snakeDirection = SnakeDirection.Right;
             _snakeParts.Add(new SnakePart() { Position = new Point(SnakeSquareSize * 5, SnakeSquareSize * 5) });
-            gameTickTimer.Interval = TimeSpan.FromMilliseconds(SnakeStartSpeed);
+            _gameTickTimer.Interval = TimeSpan.FromMilliseconds(SnakeStartSpeed);
 
-            // Draw the snake  
+            // Draw the snake & first food 
             DrawSnake();
+            DrawSnakeFood();
 
             // Starts off the timer to make the snake move         
-            gameTickTimer.IsEnabled = true;
+            _gameTickTimer.IsEnabled = true;
         }
+
+        #endregion
+
+        #region Food
+
+        private Point GetNextFoodPosition()
+        {
+            int maxX = (int)(GameArea.ActualWidth / SnakeSquareSize);
+            int maxY = (int)(GameArea.ActualHeight / SnakeSquareSize);
+            int foodX = _rnd.Next(0, maxX) * SnakeSquareSize;
+            int foodY = _rnd.Next(0, maxY) * SnakeSquareSize;
+
+            foreach (SnakePart snakePart in _snakeParts)
+            {
+                if ((snakePart.Position.X == foodX) && (snakePart.Position.Y == foodY))
+                    return GetNextFoodPosition();
+            }
+
+            return new Point(foodX, foodY);
+        }
+
+        private void DrawSnakeFood()
+        {
+            Point foodPosition = GetNextFoodPosition();
+            _snakeFood = new Ellipse()
+            {
+                Width = SnakeSquareSize,
+                Height = SnakeSquareSize,
+                Fill = _foodColor
+            };
+            GameArea.Children.Add(_snakeFood);
+            Canvas.SetTop(_snakeFood, foodPosition.Y);
+            Canvas.SetLeft(_snakeFood, foodPosition.X);
+        }
+
+        #endregion
     }
 }
